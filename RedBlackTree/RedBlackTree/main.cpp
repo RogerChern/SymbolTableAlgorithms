69e48968989f136040629c1fdb45da07ee9b4d16
RogerChern <chenyuntao08@gmail.com>
2014-09-06 15:37:48 +0800

    init commit

---
+++ RedBlackTree/RedBlackTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RedBlackTree
+//
+//  Created by RogerChen on 14-9-6.
+//  Copyright (c) 2014年 RogerChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
